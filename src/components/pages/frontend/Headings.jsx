07a6847b88f15717b8e6f0f5f02f290fb1acd5a6
Rundyson <0321-3589@lspu.edu.ph>
2025-12-03 21:18:38 +0800

    Update Headings.jsx

--- src/components/pages/frontend/Headings.jsx
+++ src/components/pages/frontend/Headings.jsx
@@ -7,7 +7,7 @@ const Headings = () => {
     <header className='bg-dark py-1'>
         <div className="container">
             <div className="flex justify-between items-center">
-                <img src={`${imgPath}/logo.png`} alt="" className='max-w-[90px]'/>
+                <img src="/img/logo.png" alt="" className='max-w-[90px]'/>
                 <nav>
                     <ul className='flex items-center gap-5 text-white'>
                         <li><NavLink>About</NavLink></li>
